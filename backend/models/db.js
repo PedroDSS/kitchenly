5c7bc309708ba0931e738bd48318ec6631a94ba0
PedroDSS <pedromiguel.dasilvasousa92@gmail.com>
2025-06-29 03:39:14 +0200

    Clean before rework

--- backend/models/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const logger = require('../utils/logger');
-
-mongoose
-  .connect(process.env.MONGODB_URI)
-  .then(() => logger.info('MongoDB connection successful!'))
-  .catch((error) => logger.error('MongoDB connection error:', error));
-
-module.exports = mongoose;
